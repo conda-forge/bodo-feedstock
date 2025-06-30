819abba87af8f5b8013f057217a90bdab223bbf9
Scott Routledge <scott@bodo.ai>
2025-06-30 15:29:31 -0400

    add debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,17 @@ export CONDA_PREFIX_OLD=$CONDA_PREFIX
 export CONDA_PREFIX=$PREFIX
 export CMAKE_GENERATOR='Ninja'
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "0" ]]; then
+    echo "CROSS COMPILING BODO!"
+    export SKBUILD_LINK_LIBPYTHON="OFF"
+    export PIP_NO_BUILD_ISOLATION=1
+    export PIP_NO_INDEX=1
+fi
+
+echo "PYTHON executable: $(which python)"
+$PYTHON -V
+$PYTHON -c "import numpy; print('NumPy version:', numpy.__version__)"
+
 $PYTHON -m pip install \
     --no-deps --no-build-isolation -vv \
     --config-settings=build.verbose=true \
