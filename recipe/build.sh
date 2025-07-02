262e7d3efadaead6d9cf05c4be80a8218e740aad
Bodo User <bodouser@bodos-mbp.mynetworksettings.com>
2025-07-02 18:35:12 -0400

    remove some constraints

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,6 @@ set -exo pipefail
 
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
 
-export CONDA_PREFIX_OLD=$CONDA_PREFIX
-export CONDA_PREFIX=$PREFIX
-export CMAKE_GENERATOR='Ninja'
-
 $PYTHON -m pip install \
     --no-deps --no-build-isolation -vv \
     --config-settings=build.verbose=true \
