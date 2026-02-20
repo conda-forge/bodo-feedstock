22bd81bf83311fd8db2c6528297b1b3d88aa07e6
Scott Routledge <scott@bodo.ai>
2026-02-20 09:35:10 -0500

    remove debugging

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ set "CONDA_PREFIX=%PREFIX%"
 set "BODO_WINDOWS_BUILD_TYPE=Release"
 
 :: Build using pip and CMake
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vv
+"%PYTHON%" -m pip install --no-deps --no-build-isolation -vv ^
+    --config-settings=build.verbose=true ^
+    --config-settings=logging.level="DEBUG" .
 
-echo PREFIX=%PREFIX%
-dir "%PREFIX%\Lib\site-packages\bodo"
